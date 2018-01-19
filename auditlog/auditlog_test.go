a2586cf9304143a75404e56e9146e069e81a020c
Anders Øen Fylling <fyllingz@gmail.com>
2018-01-19 05:30:28 +0100

    moved auditlog to subpackage & improved testing

---
+++ auditlog/auditlog_test.go
@@ -0,0 +1,17 @@
+package auditlog
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/andersfylling/disgord/testutil"
+)
+
+func TestConvertAuditLogParamsToStr(t *testing.T) {
+	data, err := ioutil.ReadFile("examples/auditlog1.json")
+	testutil.Check(err, t)
+
+	v := AuditLog{}
+	err = testutil.ValidateJSONMarshalling(data, &v)
+	testutil.Check(err, t)
+}
