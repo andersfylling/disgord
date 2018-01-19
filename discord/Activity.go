9cc610e4ca3a7b056fcf2b319991a6909398e8fa
Anders Øen Fylling <fyllingz@gmail.com>
2018-01-19 05:29:49 +0100

    moved some types into unspecified subpackage(discord) due to dependency issues

---
+++ discord/Activity.go
@@ -0,0 +1,3 @@
+package discord
+
+type Activity struct{}
---
+++ discord/Presence.go
@@ -0,0 +1,25 @@
+package discord
+
+import "github.com/andersfylling/snowflake"
+import "github.com/andersfylling/disgord/user"
+
+type Presence struct {
+	User    *user.User     `json:"user"`
+	Roles   []snowflake.ID `json:"roles"`
+	Game    *Activity      `json:"activty"`
+	GuildID snowflake.ID   `json:"guild_id"`
+	Status  string         `json:"status"`
+}
+
+func NewPresence() *Presence {
+	return &Presence{}
+}
+
+func (p *Presence) Update(status string) {
+	// Update the presence.
+	// talk to the discord api
+}
+
+func (p *Presence) String() string {
+	return p.Status
+}
