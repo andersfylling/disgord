f8d143a2268c17c73f3c018858dbd49186a369e5
Anders Øen Fylling <fyllingz@gmail.com>
2018-01-28 08:43:34 +0100

    changed listener interface for events

--- event/event.go
+++ event/event.go
@@ -1,6 +1,6 @@
 package event
 
-import "github.com/andersfylling/disgord/guild"
+import "github.com/sirupsen/logrus"
 
 // EventDispatcher is an application-level type for handling discord requests.
 // All callbacks are optional, and whether they are defined or not
@@ -28,26 +28,85 @@ type DispatcherInterface interface {
 }
 
 func NewDispatcher() *Dispatcher {
-	return &Dispatcher{
-		listeners: make(map[Type]([]interface{})),
+	d := &Dispatcher{
+		listeners: make(map[Type]CallbackStackInterface),
 	}
+
+	// add all the callback stacks
+	// socket
+	d.listeners[Ready] = &ReadyCallbackStack{}
+	d.listeners[Resumed] = &ResumeCallbackStack{}
+
+	// channel
+	d.listeners[ChannelCreate] = &ChannelCreateCallbackStack{}
+	d.listeners[ChannelUpdate] = &ChannelUpdateCallbackStack{}
+	d.listeners[ChannelDelete] = &ChannelDeleteCallbackStack{}
+	d.listeners[ChannelPinsUpdate] = &ChannelPinsUpdateCallbackStack{}
+
+	// Guild in general
+	d.listeners[GuildCreate] = &GuildCreateCallbackStack{}
+	d.listeners[GuildUpdate] = &GuildUpdateCallbackStack{}
+	d.listeners[GuildDelete] = &GuildDeleteCallbackStack{}
+	d.listeners[GuildBanAdd] = &GuildBanAddCallbackStack{}
+	d.listeners[GuildBanRemove] = &GuildBanRemoveCallbackStack{}
+	d.listeners[GuildEmojisUpdate] = &GuildEmojisUpdateCallbackStack{}
+	d.listeners[GuildIntegrationsUpdate] = &GuildIntegrationsUpdateCallbackStack{}
+
+	// Guild Member
+	d.listeners[GuildMemberAdd] = &GuildMemberAddCallbackStack{}
+	d.listeners[GuildMemberRemove] = &GuildMemberRemoveCallbackStack{}
+	d.listeners[GuildMemberUpdate] = &GuildMemberUpdateCallbackStack{}
+	d.listeners[GuildMemberChunk] = &GuildMemberChunkCallbackStack{}
+
+	// Guild role
+	d.listeners[GuildRoleCreate] = &GuildRoleCreateCallbackStack{}
+	d.listeners[GuildRoleUpdate] = &GuildRoleUpdateCallbackStack{}
+	d.listeners[GuildRoleDelete] = &GuildRoleDeleteCallbackStack{}
+
+	// message
+	d.listeners[MessageCreate] = &MessageCreateCallbackStack{}
+	d.listeners[MessageUpdate] = &MessageUpdateCallbackStack{}
+	d.listeners[MessageDelete] = &MessageDeleteCallbackStack{}
+	d.listeners[MessageDeleteBulk] = &MessageDeleteBulkCallbackStack{}
+
+	// message reaction
+	d.listeners[MessageReactionAdd] = &MessageReactionAddCallbackStack{}
+	d.listeners[MessageReactionRemove] = &MessageReactionRemoveCallbackStack{}
+	d.listeners[MessageReactionRemoveAll] = &MessageReactionRemoveAllCallbackStack{}
+
+	// presence
+	d.listeners[PresenceUpdate] = &PresenceUpdateCallbackStack{}
+
+	// typing start
+	d.listeners[TypingStart] = &TypingStartCallbackStack{}
+
+	// user update
+	d.listeners[UserUpdate] = &UserUpdateCallbackStack{}
+
+	// voice
+	d.listeners[VoiceStateUpdate] = &VoiceStateUpdateCallbackStack{}
+	d.listeners[VoiceServerUpdate] = &VoiceServerUpdateCallbackStack{}
+
+	// webhook
+	d.listeners[WebhooksUpdate] = &WebhooksUpdateCallbackStack{}
+
+	return d
 }
 
 type Dispatcher struct {
-	listeners map[Type]([]interface{})
+	listeners map[Type]CallbackStackInterface
 }
 
 func (d *Dispatcher) OnEvent(eventName Type, listener interface{}) {
-	d.listeners[eventName] = append(d.listeners[eventName], listener)
+	if listeners, ok := d.listeners[eventName]; ok {
+		listeners.Add(listener)
+	} else {
+		logrus.Errorf("no callback interface registered for `%s`", eventName)
+	}
 }
 
 func (d *Dispatcher) Trigger(eventName Type, params ...interface{}) {
-	if val, ok := d.listeners[eventName]; ok {
-		for _, listener := range val {
-			switch l := listener.(type) {
-			case func(guild *guild.Guild):
-				l(params[0].(*guild.Guild))
-			}
-		}
+	if listeners, ok := d.listeners[eventName]; ok {
+		listeners.Trigger(params)
 	}
 }
