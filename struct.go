b1a160cb60907fbb55bd3c23ca714734a48d551c
Anders Øen Fylling <anders@nordic.email>
2018-09-13 12:33:26 +0200

    removes redundant files

--- struct.go
+++ struct.go
@@ -1,5 +1,11 @@
 package disgord
 
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
 // common functionality/types used by struct_*.go files goes here
 
 // Copier holds the CopyOverTo method which copies all it's content from one
@@ -55,4 +61,123 @@ type discordDeleter interface {
 // any struct.
 type DeepCopier interface {
 	DeepCopy() interface{}
+}
+
+// Discord types
+
+// helperTypes: timestamp, levels, etc.
+
+// discordTimeFormat to be able to correctly convert timestamps back into json,
+// we need the micro timestamp with an addition at the ending.
+// time.RFC3331 does not yield an output similar to the discord timestamp input, the date is however correct.
+const timestampFormat = "2006-01-02T15:04:05.000000+00:00"
+
+type Marshaler interface {
+	MarshalJSON() ([]byte, error)
+}
+
+type Unmarshaler interface {
+	UnmarshalJSON(data []byte) error
+}
+
+type Timestamp time.Time
+
+// error: https://stackoverflow.com/questions/28464711/go-strange-json-hyphen-unmarshall-error
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	// wrap in double qoutes for valid json parsing
+	jsonReady := fmt.Sprintf("\"%s\"", t.String())
+
+	return []byte(jsonReady), nil
+}
+
+func (t *Timestamp) UnmarshalJSON(data []byte) error {
+	var ts time.Time
+	err := json.Unmarshal(data, &ts)
+	if err != nil {
+		return err
+	}
+
+	*t = Timestamp(ts)
+	return nil
+}
+
+// String converts the timestamp into a discord formatted timestamp. time.RFC3331 does not suffice
+func (t Timestamp) String() string {
+	return t.Time().Format(timestampFormat)
+}
+
+// Time converts the DiscordTimestamp into a time.Time type.......
+func (t Timestamp) Time() time.Time {
+	return time.Time(t)
+}
+
+// -----------
+// levels
+
+// ExplicitContentFilterLvl ...
+// https://discordapp.com/developers/docs/resources/guild#guild-object-explicit-content-filter-level
+type ExplicitContentFilterLvl uint
+
+func (ecfl *ExplicitContentFilterLvl) Disabled() bool {
+	return *ecfl == 0
+}
+func (ecfl *ExplicitContentFilterLvl) MembersWithoutRoles() bool {
+	return *ecfl == 1
+}
+func (ecfl *ExplicitContentFilterLvl) AllMembers() bool {
+	return *ecfl == 2
+}
+
+// MFA ...
+// https://discordapp.com/developers/docs/resources/guild#guild-object-mfa-level
+type MFALvl uint
+
+func (mfal *MFALvl) None() bool {
+	return *mfal == 0
+}
+func (mfal *MFALvl) Elevated() bool {
+	return *mfal == 1
+}
+
+// Verification ...
+// https://discordapp.com/developers/docs/resources/guild#guild-object-verification-level
+type VerificationLvl uint
+
+// None unrestricted
+func (vl *VerificationLvl) None() bool {
+	return *vl == 0
+}
+
+// Low must have verified email on account
+func (vl *VerificationLvl) Low() bool {
+	return *vl == 1
+}
+
+// Medium must be registered on Discord for longer than 5 minutes
+func (vl *VerificationLvl) Medium() bool {
+	return *vl == 2
+}
+
+// High (╯°□°）╯︵ ┻━┻ - must be a member of the server for longer than 10 minutes
+func (vl *VerificationLvl) High() bool {
+	return *vl == 3
+}
+
+// VeryHigh ┻━┻ミヽ(ಠ益ಠ)ﾉ彡┻━┻ - must have a verified phone number
+func (vl *VerificationLvl) VeryHigh() bool {
+	return *vl == 4
+}
+
+// DefaultMessageNotification ...
+// https://discordapp.com/developers/docs/resources/guild#guild-object-default-message-notification-level
+type DefaultMessageNotificationLvl uint
+
+func (dmnl *DefaultMessageNotificationLvl) AllMessages() bool {
+	return *dmnl == 0
+}
+func (dmnl *DefaultMessageNotificationLvl) OnlyMentions() bool {
+	return *dmnl == 1
+}
+func (dmnl *DefaultMessageNotificationLvl) Equals(v uint) bool {
+	return uint(*dmnl) == v
 }
--- struct_test.go
+++ struct_test.go
@@ -1,6 +1,86 @@
 package disgord
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+// ValidateJSONMarshalling
+func validateJSONMarshalling(b []byte, v interface{}) error {
+	var err error
+
+	// convert to struct
+	err = json.Unmarshal(b, &v)
+	if err != nil {
+		return err
+	}
+
+	// back to json
+	prettyJSON, err := json.MarshalIndent(&v, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	// sort the data by keys
+	// omg im getting lost in my own train of thought
+	omg := make(map[string]interface{})
+	err = json.Unmarshal(prettyJSON, &omg)
+	if err != nil {
+		return err
+	}
+
+	omgAgain := make(map[string]interface{})
+	err = json.Unmarshal(b, &omgAgain)
+	if err != nil {
+		return err
+	}
+
+	// back to json
+	prettyJSON, err = json.MarshalIndent(&omg, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	b, err = json.MarshalIndent(&omgAgain, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	// minify for comparison
+	dst1 := bytes.Buffer{}
+	err = json.Compact(&dst1, b)
+	if err != nil {
+		return err
+	}
+	dst2 := bytes.Buffer{}
+	err = json.Compact(&dst2, prettyJSON)
+	if err != nil {
+		return err
+	}
+
+	// compare
+	if dst2.String() != dst1.String() {
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(string(b), string(prettyJSON), false)
+		return fmt.Errorf("json data differs. \nDifference \n%s", dmp.DiffPrettyText(diffs))
+	}
+
+	return nil
+}
+
+func check(err error, t *testing.T) {
+	// Hide function from stacktrace, PR#3
+	t.Helper()
+
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestError_InterfaceImplementations(t *testing.T) {
 	var u interface{} = &ErrorUnsupportedType{}
@@ -8,6 +88,103 @@ func TestError_InterfaceImplementations(t *testing.T) {
 	t.Run("error", func(t *testing.T) {
 		if _, ok := u.(error); !ok {
 			t.Error("ErrorUnsupportedType does not implement error")
+		}
+	})
+}
+
+// unmarshalling
+
+func BenchmarkUnmarshalReflection(b *testing.B) {
+	data, err := ioutil.ReadFile("testdata/user/user1.json")
+	if err != nil {
+		b.Skip("missing file for benchmarking unmarshal")
+		return
+	}
+
+	b.Run("using reflection", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			var user *User
+			json.Unmarshal(data, user)
+		}
+	})
+
+	b.Run("using interface wiring", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			user := &User{}
+			var m = make(map[string]interface{})
+			json.Unmarshal(data, m)
+
+			var v interface{}
+			var ok bool
+
+			if v, ok = m["id"]; ok {
+				user.ID = v.(Snowflake)
+			}
+			if v, ok = m["username"]; ok {
+				user.Username = v.(string)
+			}
+			if v, ok = m["discriminator"]; ok {
+				user.Discriminator = v.(string)
+			}
+			if v, ok = m["email"]; ok {
+				user.Email = v.(string)
+			}
+			if v, ok = m["avatar"]; ok {
+				user.Avatar = v.(*string)
+			}
+			if v, ok = m["token"]; ok {
+				user.Token = v.(string)
+			}
+			if v, ok = m["verified"]; ok {
+				user.Verified = v.(bool)
+			}
+			if v, ok = m["mfa_enabled"]; ok {
+				user.MFAEnabled = v.(bool)
+			}
+			if v, ok = m["bot"]; ok {
+				user.Bot = v.(bool)
+			}
+
+		}
+	})
+
+	b.Run("using string wiring", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			user := &User{}
+			var m = make(map[string]string)
+			json.Unmarshal(data, m)
+
+			var v string
+			var ok bool
+
+			if v, ok = m["id"]; ok {
+				user.ID = ParseSnowflakeString(v)
+			}
+			if v, ok = m["username"]; ok {
+				user.Username = v
+			}
+			if v, ok = m["discriminator"]; ok {
+				user.Discriminator = v
+			}
+			if v, ok = m["email"]; ok {
+				user.Email = v
+			}
+			if vv, ok := m["avatar"]; ok {
+				user.Avatar = &vv
+			}
+			if v, ok = m["token"]; ok {
+				user.Token = v
+			}
+			if v, ok = m["verified"]; ok {
+				user.Verified = v == "true"
+			}
+			if v, ok = m["mfa_enabled"]; ok {
+				user.MFAEnabled = v == "true"
+			}
+			if v, ok = m["bot"]; ok {
+				user.Bot = v == "true"
+			}
+
 		}
 	})
 }
