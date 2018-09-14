43e53cabd5925261e8803c65fe3641f75d9ef225
Anders Øen Fylling <anders@nordic.email>
2018-09-14 19:15:36 +0200

    adds DeepCopier, Copier to Webhook

--- struct_webhook.go
+++ struct_webhook.go
@@ -1,8 +1,12 @@
 package disgord
 
+import "sync"
+
 // Webhook Used to represent a webhook
 // https://discordapp.com/developers/docs/resources/webhook#webhook-object
 type Webhook struct {
+	sync.RWMutex `json:"-"`
+
 	ID        Snowflake `json:"id"`                 //  |
 	GuildID   Snowflake `json:"guild_id,omitempty"` //  |?
 	ChannelID Snowflake `json:"channel_id"`         //  |
@@ -10,4 +14,35 @@ type Webhook struct {
 	Name      string    `json:"name"`               //  |?
 	Avatar    string    `json:"avatar"`             //  |?
 	Token     string    `json:"token"`              //  |
+}
+
+func (w *Webhook) DeepCopy() (copy interface{}) {
+	copy = &Webhook{}
+	w.CopyOverTo(copy)
+
+	return
+}
+
+func (w *Webhook) CopyOverTo(other interface{}) (err error) {
+	var ok bool
+	var hook *Webhook
+	if hook, ok = other.(*Webhook); !ok {
+		err = NewErrorUnsupportedType("given interface{} was not of type *Webhook")
+		return
+	}
+
+	w.RLock()
+	hook.Lock()
+
+	hook.ID = w.ID
+	hook.GuildID = w.GuildID
+	hook.ChannelID = w.ChannelID
+	hook.User = w.User.DeepCopy().(*User)
+	hook.Name = w.Name
+	hook.Avatar = w.Avatar
+	hook.Token = w.Token
+
+	w.RUnlock()
+	hook.Unlock()
+	return
 }
