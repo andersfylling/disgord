16c53e3ba60939d713c1cb2bd5165c0c0a3a86d9
Anders Øen Fylling <anders@nordic.email>
2018-09-15 19:32:27 +0200

    :white_check_mark:tests DeepCopier & Copier for Audit log structs

--- struct_auditlog_test.go
+++ struct_auditlog_test.go
@@ -13,3 +13,62 @@ func TestAuditLogConvertAuditLogParamsToStr(t *testing.T) {
 	err = validateJSONMarshalling(data, &v)
 	check(err, t)
 }
+
+func TestAuditLog_InterfaceImplementations(t *testing.T) {
+	t.Run("AuditLog", func(t *testing.T) {
+		var u interface{} = &AuditLog{}
+		t.Run("DeepCopier", func(t *testing.T) {
+			if _, ok := u.(DeepCopier); !ok {
+				t.Error("does not implement DeepCopier")
+			}
+		})
+
+		t.Run("Copier", func(t *testing.T) {
+			if _, ok := u.(Copier); !ok {
+				t.Error("does not implement Copier")
+			}
+		})
+	})
+	t.Run("AuditLogEntry", func(t *testing.T) {
+		var u interface{} = &AuditLogEntry{}
+		t.Run("DeepCopier", func(t *testing.T) {
+			if _, ok := u.(DeepCopier); !ok {
+				t.Error("does not implement DeepCopier")
+			}
+		})
+
+		t.Run("Copier", func(t *testing.T) {
+			if _, ok := u.(Copier); !ok {
+				t.Error("does not implement Copier")
+			}
+		})
+	})
+	t.Run("AuditLogOption", func(t *testing.T) {
+		var u interface{} = &AuditLogOption{}
+		t.Run("DeepCopier", func(t *testing.T) {
+			if _, ok := u.(DeepCopier); !ok {
+				t.Error("does not implement DeepCopier")
+			}
+		})
+
+		t.Run("Copier", func(t *testing.T) {
+			if _, ok := u.(Copier); !ok {
+				t.Error("does not implement Copier")
+			}
+		})
+	})
+	t.Run("AuditLogChange", func(t *testing.T) {
+		var u interface{} = &AuditLogChange{}
+		t.Run("DeepCopier", func(t *testing.T) {
+			if _, ok := u.(DeepCopier); !ok {
+				t.Error("does not implement DeepCopier")
+			}
+		})
+
+		t.Run("Copier", func(t *testing.T) {
+			if _, ok := u.(Copier); !ok {
+				t.Error("does not implement Copier")
+			}
+		})
+	})
+}
