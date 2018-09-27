0ceacc02f4e34b8a4446a671f9290c6fed1032e7
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2018-09-27 16:41:05 +0200

    📝💚 Added comment to Unmarshal method

--- httd/json_std.go
+++ httd/json_std.go
@@ -4,6 +4,7 @@ package httd
 
 import "encoding/json"
 
+// Unmarshal is the json unmarshalling implementation that is defined by the used build tags.
 func Unmarshal(data []byte, v interface{}) error {
 	if j, has := v.(json.Unmarshaler); has {
 		return j.UnmarshalJSON(data)
