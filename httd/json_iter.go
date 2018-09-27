ea25df1d8963eb3ac92b0b1143c0328d4668d75e
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2018-09-27 16:41:05 +0200

    📝💚 Added comment to Unmarshal method

--- httd/json_iter.go
+++ httd/json_iter.go
@@ -8,6 +8,7 @@ import (
 	"github.com/json-iterator/go"
 )
 
+// Unmarshal is the json unmarshalling implementation that is defined by the used build tags.
 func Unmarshal(data []byte, v interface{}) error {
 	if j, has := v.(json.Unmarshaler); has {
 		return j.UnmarshalJSON(data)
