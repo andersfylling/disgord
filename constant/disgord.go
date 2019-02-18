9175fe9e1f92c74b299d487f6b8a05fc8175fb03
Anders Fylling <anders@nordic.email>
2019-02-18 03:52:20 +0100

    updates version const

--- constant/disgord.go
+++ constant/disgord.go
@@ -8,4 +8,4 @@ const Name = "DisGord"
 // Version project version
 // TODO: git hook which creates a new git tag after a commit, given that
 //        the version here has changed or does not exist as a git tag yet.
-const Version = "v0.9.6"
+const Version = "v0.10.0"
