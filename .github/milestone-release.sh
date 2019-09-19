12689151500f959e54061ef3478b2902fe81e511
Anders Fylling <anders@nordic.email>
2019-09-19 07:21:56 +0200

    auto release on milestone close

---
+++ .github/milestone-release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# github REST commands
+wget https://raw.githubusercontent.com/whiteinge/ok.sh/master/ok.sh -O gith
+chmod +x gith
+
+# get last closed milestone (yes this is a race cond)
+./gith list_milestones andersfylling/disgord state=closed sort=closed_at direction=desc > closed_milestones.txt
+VERSION="$(awk 'NR==1 {print $3}' "closed_milestones.txt")"
+if [[ ! ${VERSION:0:1} == "v" ]] ; then
+  >&2 echo "ERROR: $(cat closed_milestones.txt)";
+  exit 1
+fi
+
+# cleanup
+rm gith
+rm "closed_milestones.txt"
+
+# setup git env
+git config user.email "anders@nordic.email"
+git config user.name "Anders Fylling (bot)"
+git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+git checkout develop
+
+# update version const
+VERSION_FILE="constant/version.go"
+echo "package constant" > "${VERSION_FILE}"
+echo "const Version = \"${VERSION}\"" >> "${VERSION_FILE}"
+go fmt ./...
+
+# generate CHANGELOG.md
+github_changelog_generator --release-branch develop -u andersfylling -p disgord
+
+# commit, tag and push
+git add .
+git commit -m "gen changelog & set version to ${VERSION}"
+git tag "${VERSION}" -m "DisGord ${VERSION}"
+(git diff --quiet && git diff --staged --quiet) || (git commit -m "${COMMIT_MSG}"; git push origin develop --tags)
