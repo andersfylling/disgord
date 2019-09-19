b734b6c0b7dfba21357f2c52283a9467c13ee7b4
Anders Fylling <anders@nordic.email>
2019-09-19 04:31:52 +0200

    set version to v0.12.0-rc5

--- constant/disgord.go
+++ constant/disgord.go
@@ -6,8 +6,3 @@ const GitHubURL = "https://github.com/andersfylling/disgord"
 const Name = "DisGord"
 
 const UserAgent = "DiscordBot (" + GitHubURL + ", " + Version + ") " // "DiscordBot (%s, %s) %s"
-
-// Version project version
-// TODO: git hook which creates a new git tag after a commit, given that
-//        the version here has changed or does not exist as a git tag yet.
-const Version = "v0.12.0-rc3"
---
+++ constant/version.go
@@ -0,0 +1,3 @@
+package constant
+
+const Version = "v0.12.0-rc5"
