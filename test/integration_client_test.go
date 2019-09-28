be0fa7723a2ce7b7fd7591e30a08169a73bab024
Anders Øen Fylling <7851860+andersfylling@users.noreply.github.com>
2019-09-28 20:37:32 +0200

    panic when registerring a incorrect handler signature (fixes #20… (#217)

---
+++ test/integration_client_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/andersfylling/disgord"
+)
+
+func TestOn(t *testing.T) {
+	c := disgord.New(&disgord.Config{
+		BotToken:     "sdkjfhdksfhskdjfhdkfjsd",
+		DisableCache: true,
+	})
+
+	t.Run("normal Session", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("should not have triggered a panic")
+			}
+		}()
+
+		c.On(disgord.EvtChannelCreate, func(s disgord.Session, e *disgord.ChannelCreate) {})
+	})
+
+	t.Run("normal Session with ctrl", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("should not have triggered a panic")
+			}
+		}()
+
+		c.On(disgord.EvtChannelCreate, func(s disgord.Session, e *disgord.ChannelCreate) {}, &disgord.Ctrl{Runs: 1})
+	})
+
+	t.Run("normal Session with multiple ctrl's", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("multiple controllers should trigger a panic")
+			}
+		}()
+
+		c.On(disgord.EvtChannelCreate,
+			func(s disgord.Session, e *disgord.ChannelCreate) {},
+			&disgord.Ctrl{Runs: 1},
+			&disgord.Ctrl{Until: time.Now().Add(1 * time.Minute)})
+	})
+
+	t.Run("Session pointer", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("Did not panic on incorrect handler signature")
+			}
+		}()
+
+		c.On(disgord.EvtChannelCreate, func(s *disgord.Session, e *disgord.ChannelCreate) {})
+	})
+}
