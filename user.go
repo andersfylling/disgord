22f45d5aff6c2ac92b87a624a88f8f051bd542c8
Anders Øen Fylling <anders@nordic.email>
2020-08-30 15:00:31 +0200

    move deprecated 'stuff' to deprecated.go

--- user.go
+++ user.go
@@ -730,37 +730,6 @@ func (c currentUserQueryBuilder) GetGuilds(params *GetCurrentUserGuildsParams, f
 	return nil, errors.New("unable to cast guild slice")
 }
 
-// GetDMChannels [REST] Returns a list of DM channel objects.
-//  Method                  GET
-//  Endpoint                /users/@me/channels
-//  Discord documentation   https://discord.com/developers/docs/resources/user#get-user-dms
-//  Reviewed                2019-02-19
-//  Comment                 Apparently Discord removed support for this in 2016 and updated their docs 2 years after..
-//							https://github.com/discord/discord-api-docs/issues/184
-//							For now I'll just leave this here, until I can do a cache lookup. Making this cache
-//							dependent.
-// Deprecated: Needs cache checking to get the actual list of Channels
-func (c currentUserQueryBuilder) GetDMChannels(flags ...Flag) (ret []*Channel, err error) {
-	r := c.client.newRESTRequest(&httd.Request{
-		Endpoint: endpoint.UserMeChannels(),
-		Ctx:      c.ctx,
-	}, flags)
-	r.factory = func() interface{} {
-		tmp := make([]*Channel, 0) // TODO: use channel pool to get enough Channels
-		return &tmp
-	}
-
-	var vs interface{}
-	if vs, err = r.Execute(); err != nil {
-		return nil, err
-	}
-
-	if chans, ok := vs.(*[]*Channel); ok {
-		return *chans, nil
-	}
-	return nil, errors.New("unable to cast guild slice")
-}
-
 // CreateGroupDMParams required JSON params for func CreateGroupDM
 // https://discord.com/developers/docs/resources/user#create-group-dm
 type CreateGroupDMParams struct {
