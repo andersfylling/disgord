ff9b7e5b8892cc1f03a9f08fec61a9280b2bbb08
Anders Øen Fylling <anders@nordic.email>
2021-01-03 02:30:41 +0100

    specify only major version

--- internal/constant/disgord.go
+++ internal/constant/disgord.go
@@ -1,8 +1,6 @@
 package constant
 
-// GitHubURL repository url
-const GitHubURL = "https://github.com/andersfylling/disgord"
-
-const Name = "Disgord"
-
-const UserAgent = "DiscordBot (" + GitHubURL + ", " + Version + ") " // "DiscordBot (%s, %s) %s"
+const (
+	GitHubURL = "https://github.com/andersfylling/disgord"
+	Name      = "Disgord"
+)
