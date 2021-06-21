56db5a1832fa32a193df135e2d33edc4d0bdd9fd
Germano Eichenberg <germanoeich@gmail.com>
2021-06-21 06:35:35 -0300

    Add available field to emoji (#408)

--- emoji.go
+++ emoji.go
@@ -27,6 +27,7 @@ type Emoji struct {
 	RequireColons bool        `json:"require_colons,omitempty"`
 	Managed       bool        `json:"managed,omitempty"`
 	Animated      bool        `json:"animated,omitempty"`
+	Available     bool        `json:"available,omitempty"`
 }
 
 var _ Reseter = (*Emoji)(nil)
