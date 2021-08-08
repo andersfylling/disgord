774e964fd206d061b3fd1642c48c7a66758bc2d6
Anders Øen Fylling <anders@nordic.email>
2021-08-08 02:48:26 +0200

    update reaction.go

--- reaction.go
+++ reaction.go
@@ -48,23 +48,24 @@ func unwrapEmoji(e string) string {
 
 type ReactionQueryBuilder interface {
 	WithContext(ctx context.Context) ReactionQueryBuilder
+	WithFlags(flags ...Flag) ReactionQueryBuilder
 
 	// Create create a reaction for the message. This endpoint requires the 'READ_MESSAGE_HISTORY'
 	// permission to be present on the current user. Additionally, if nobody else has reacted to the message using this
 	// emoji, this endpoint requires the 'ADD_REACTIONS' permission to be present on the current user. Returns a 204
 	// empty response on success. The maximum request size when sending a message is 8MB.
-	Create(flags ...Flag) (err error)
+	Create() (err error)
 
 	// Get Get a list of Users that reacted with this emoji. Returns an array of user objects on success.
-	Get(params URLQueryStringer, flags ...Flag) (reactors []*User, err error)
+	Get(params URLQueryStringer) (reactors []*User, err error)
 
 	// DeleteOwn Delete a reaction the current user has made for the message.
 	// Returns a 204 empty response on success.
-	DeleteOwn(flags ...Flag) (err error)
+	DeleteOwn() (err error)
 
 	// DeleteUser Deletes another user's reaction. This endpoint requires the 'MANAGE_MESSAGES' permission
 	// to be present on the current user. Returns a 204 empty response on success.
-	DeleteUser(userID Snowflake, flags ...Flag) (err error)
+	DeleteUser(userID Snowflake) (err error)
 }
 
 func (m messageQueryBuilder) Reaction(emoji interface{}) ReactionQueryBuilder {
@@ -73,6 +74,7 @@ func (m messageQueryBuilder) Reaction(emoji interface{}) ReactionQueryBuilder {
 
 type reactionQueryBuilder struct {
 	ctx    context.Context
+	flags  Flag
 	client *Client
 	cid    Snowflake
 	mid    Snowflake
@@ -81,6 +83,11 @@ type reactionQueryBuilder struct {
 
 func (r reactionQueryBuilder) WithContext(ctx context.Context) ReactionQueryBuilder {
 	r.ctx = ctx
+	return &r
+}
+
+func (r reactionQueryBuilder) WithFlags(flags ...Flag) ReactionQueryBuilder {
+	r.flags = mergeFlags(flags)
 	return &r
 }
 
@@ -93,7 +100,7 @@ func (r reactionQueryBuilder) WithContext(ctx context.Context) ReactionQueryBuil
 //  Discord documentation   https://discord.com/developers/docs/resources/channel#create-reaction
 //  Reviewed                2019-01-30
 //  Comment                 emoji either unicode (string) or *Emoji with an snowflake Snowflake if it's custom
-func (r reactionQueryBuilder) Create(flags ...Flag) error {
+func (r reactionQueryBuilder) Create() error {
 	if r.cid.IsZero() {
 		return errors.New("channelID must be set to target the correct channel")
 	}
@@ -113,7 +120,7 @@ func (r reactionQueryBuilder) Create(flags ...Flag) error {
 		Method:   httd.MethodPut,
 		Endpoint: endpoint.ChannelMessageReactionMe(r.cid, r.mid, emojiCode),
 		Ctx:      r.ctx,
-	}, flags)
+	}, r.flags)
 
 	_, err = req.Execute()
 	return err
@@ -126,7 +133,7 @@ func (r reactionQueryBuilder) Create(flags ...Flag) error {
 //  Discord documentation   https://discord.com/developers/docs/resources/channel#delete-own-reaction
 //  Reviewed                2019-01-28
 //  Comment                 emoji either unicode (string) or *Emoji with an snowflake Snowflake if it's custom
-func (r reactionQueryBuilder) DeleteOwn(flags ...Flag) error {
+func (r reactionQueryBuilder) DeleteOwn() error {
 	if r.cid.IsZero() {
 		return errors.New("channelID must be set to target the correct channel")
 	}
@@ -146,7 +153,7 @@ func (r reactionQueryBuilder) DeleteOwn(flags ...Flag) error {
 		Method:   httd.MethodDelete,
 		Endpoint: endpoint.ChannelMessageReactionMe(r.cid, r.mid, emojiCode),
 		Ctx:      r.ctx,
-	}, flags)
+	}, r.flags)
 
 	_, err = req.Execute()
 	return err
@@ -159,7 +166,7 @@ func (r reactionQueryBuilder) DeleteOwn(flags ...Flag) error {
 //  Discord documentation   https://discord.com/developers/docs/resources/channel#delete-user-reaction
 //  Reviewed                2019-01-28
 //  Comment                 emoji either unicode (string) or *Emoji with an snowflake Snowflake if it's custom
-func (r reactionQueryBuilder) DeleteUser(userID Snowflake, flags ...Flag) error {
+func (r reactionQueryBuilder) DeleteUser(userID Snowflake) error {
 	if r.cid.IsZero() {
 		return errors.New("channelID must be set to target the correct channel")
 	}
@@ -182,7 +189,7 @@ func (r reactionQueryBuilder) DeleteUser(userID Snowflake, flags ...Flag) error 
 		Method:   httd.MethodDelete,
 		Endpoint: endpoint.ChannelMessageReactionUser(r.cid, r.mid, emojiCode, userID),
 		Ctx:      r.ctx,
-	}, flags)
+	}, r.flags)
 
 	_, err = req.Execute()
 	return err
@@ -203,7 +210,7 @@ var _ URLQueryStringer = (*GetReactionURLParams)(nil)
 //  Discord documentation   https://discord.com/developers/docs/resources/channel#get-reactions
 //  Reviewed                2019-01-28
 //  Comment                 emoji either unicode (string) or *Emoji with an snowflake Snowflake if it's custom
-func (r reactionQueryBuilder) Get(params URLQueryStringer, flags ...Flag) (ret []*User, err error) {
+func (r reactionQueryBuilder) Get(params URLQueryStringer) (ret []*User, err error) {
 	if r.cid.IsZero() {
 		return nil, errors.New("channelID must be set to target the correct channel")
 	}
@@ -227,7 +234,7 @@ func (r reactionQueryBuilder) Get(params URLQueryStringer, flags ...Flag) (ret [
 	req := r.client.newRESTRequest(&httd.Request{
 		Endpoint: endpoint.ChannelMessageReaction(r.cid, r.mid, emojiCode) + query,
 		Ctx:      r.ctx,
-	}, flags)
+	}, r.flags)
 	req.factory = func() interface{} {
 		tmp := make([]*User, 0)
 		return &tmp
