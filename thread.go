1e65dd3b85c64cbbea470f049a2b83d20d4db8b0
jonathan-rizk <jonrizk@protonmail.com>
2021-11-21 12:31:59 -0600

    added json descriptions

--- thread.go
+++ thread.go
@@ -2,17 +2,17 @@ package disgord
 
 // ref https://discord.com/developers/docs/resources/channel#thread-member-object
 type ThreadMember struct {
-	ID Snowflake
-	UserID Snowflake
-	JoinTimestamp Time
-	Flags Flag
+	ID Snowflake `json:"id,omitempty"`
+	UserID Snowflake `json:"user_id,omitempty"`
+	JoinTimestamp Time `json:"join_timestamp"`
+	Flags Flag `json:"flags"`
 }
 
 // ref https://discord.com/developers/docs/resources/channel#thread-metadata-object
 type ThreadMetadata struct {
-	Archived bool
-	AutoArchiveDuration	int
-	ArchiveTimestamp Time
-	Locked bool
-	Invitable bool
+	Archived bool `json:"archived"`
+	AutoArchiveDuration	int `json:"auto_archive_duration"`
+	ArchiveTimestamp Time `json:"archive_timestamp"`
+	Locked bool `json:"locked"`
+	Invitable bool `json:"inviteable,omitempty"`
 }
