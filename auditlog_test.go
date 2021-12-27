01344056bee274c262ceaf4a1c0080f31f4e7288
Anders Øen Fylling <anders@nordic.email>
2021-12-27 18:00:45 +0100

    remove old auditlogs code

--- auditlog_test.go
+++ auditlog_test.go
@@ -4,13 +4,7 @@
 package disgord
 
 import (
-	"context"
-	"errors"
-	"net/http"
 	"testing"
-
-	"github.com/andersfylling/disgord/internal/endpoint"
-	"github.com/andersfylling/disgord/internal/httd"
 )
 
 func TestAuditLog_InterfaceImplementations(t *testing.T) {
@@ -69,115 +63,5 @@ func TestAuditLog_InterfaceImplementations(t *testing.T) {
 				t.Error("does not implement Copier")
 			}
 		})
-	})
-}
-
-func TestAuditLog(t *testing.T) {
-	params := &guildAuditLogsBuilder{}
-	params.r.setup(nil, nil, nil)
-	var wants string
-
-	wants = ""
-	verifyQueryString(t, params.r.urlParams, wants)
-
-	s := "438543957"
-	ss, _ := GetSnowflake(s)
-	params.SetUserID(ss)
-	wants = "?user_id=" + s
-	verifyQueryString(t, params.r.urlParams, wants)
-
-	params.SetActionType(6)
-	wants += "&action_type=6"
-	wantsAlternative := "?action_type=6&user_id=" + s
-	got := params.r.urlParams.URLQueryString()
-	if !(wants == got || wantsAlternative == got) {
-		t.Errorf("incorrect query param string. Got '%s', wants '%s' or '%s'", params.r.urlParams.URLQueryString(), wants, wantsAlternative)
-	}
-
-	params.SetActionType(0)
-	wants = "?user_id=" + s + "&action_type=0"
-	wantsAlternative = "?action_type=0&user_id=" + s
-	got = params.r.urlParams.URLQueryString()
-	if !(wants == got || wantsAlternative == got) {
-		t.Errorf("incorrect query param string. Got '%s', wants '%s' or '%s'", params.r.urlParams.URLQueryString(), wants, wantsAlternative)
-	}
-}
-
-func TestGuildAuditLogs(t *testing.T) {
-	t.Run("configuration", func(t *testing.T) {
-		// successfull response
-		client := &reqMocker{
-			body: []byte(`{}`),
-			resp: &http.Response{
-				StatusCode: 200,
-			},
-		}
-
-		builder := &guildAuditLogsBuilder{}
-		builder.r.itemFactory = auditLogFactory
-		builder.r.IgnoreCache().setup(client, &httd.Request{
-			Method:   http.MethodGet,
-			Endpoint: endpoint.GuildAuditLogs(Snowflake(7)),
-			Ctx:      context.Background(),
-		}, nil)
-
-		_, err := builder.Execute()
-		if err != nil {
-			t.Error(err)
-		}
-
-		if client.req.Endpoint != "/guilds/7/audit-logs" {
-			t.Error("incorrect endpoint: ", client.req.Endpoint)
-		}
-	})
-	t.Run("missing-permission", func(t *testing.T) {
-		errorMsg := "missing permission flag?"
-		client := &reqMocker{
-			body: []byte(`{"code":403,"message":"` + errorMsg + `"}`),
-			resp: &http.Response{
-				StatusCode: 403,
-			},
-			err: errors.New("permission issue"),
-		}
-
-		builder := &guildAuditLogsBuilder{}
-		builder.r.IgnoreCache().setup(client, &httd.Request{
-			Method:   http.MethodGet,
-			Endpoint: endpoint.GuildAuditLogs(Snowflake(7)),
-			Ctx:      context.Background(),
-		}, nil)
-
-		logs, err := builder.Execute()
-		if logs != nil {
-			t.Error("expected logs to be nil (not initiated)")
-		}
-		if err == nil {
-			t.Fatal("expected an error, got nil")
-		}
-
-		// TODO: implement ErrREST check
-	})
-	t.Run("no-content", func(t *testing.T) {
-		client := &reqMocker{
-			body: []byte(`{}`),
-			resp: &http.Response{
-				StatusCode: 204,
-			},
-		}
-
-		builder := &guildAuditLogsBuilder{}
-		builder.r.itemFactory = auditLogFactory
-		builder.r.IgnoreCache().setup(client, &httd.Request{
-			Method:   http.MethodGet,
-			Endpoint: endpoint.GuildAuditLogs(Snowflake(7)),
-			Ctx:      context.Background(),
-		}, nil)
-
-		_, err := builder.Execute()
-		if err != nil {
-			t.Fatal("unexpected error: " + err.Error())
-		}
-
-		// TODO: implement ErrREST check
 	})
 }
